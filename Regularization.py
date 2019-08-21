9f5043dc37c73539beab1b33b8fdd9caa68ec80f
Jeremy Lefort-Besnard <21123003+JLefortBesnard@users.noreply.github.com>
2019-08-21 09:12:30 +0800

    Update Regularization.py

--- Regularization.py
+++ Regularization.py
@@ -1,64 +1,95 @@
+
 import numpy as np
 import pandas as pd
 
-
+# reproducible results
 np.random.seed(42)
+
+####################
+### create data  ###
+####################
 
 data = pd.DataFrame(np.random.normal(2, 1, 200), # n = 100
                     columns = ["X1"])
-                    
 np.random.randint(1, 10)
 for i in range(2, 1001): # p = 1000
+    # define an input (Xs)
     X = np.random.normal(np.random.randint(3, 10), np.random.randint(1, 3), 200)
     name = "X{}".format(i)
     data[name] = X
 
-random_betas = np.random.randint(5, 20, 20)  
+# create underlying truth with 20 varibales informative
+# define betas coeficients
+random_betas = np.random.randint(5, 20, 20)
+# apply coefs to inputs
 data_betas = data.loc[:, :"X20"] * random_betas
-Y_ = data_betas.sum(axis=1)  # create underlying truth with all variables informative
-data["Y"] = Y_ # all variables are informative
+# define output based on the 20 varibales and their artificial coefficents
+data["Y"] = data_betas.sum(axis=1)
 
 
-# apply the ridge and lasso and return accuracy
+
+##################################################################
+### define specific dataset of p=20 with 20 important features ###
+##################################################################
+print("p = 20 with: 20 features are important")
+X = data.loc[:, :"X20"].values
+Y = data["Y"]
+
+### Training and testing for each model ###
+
+# apply a linear regression, a linear regression with univaritate feature selection,
+# a ridge regression and a lasso
+from sklearn.feature_selection import SelectPercentile, f_classif
 from sklearn import linear_model
-from sklearn import cross_validation
+from sklearn.model_selection import KFold
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 LinReg = linear_model.LinearRegression()
 Ridge = linear_model.RidgeCV()
 Lasso = linear_model.LassoCV()
 
-
-
-print("p = 20, 20 features are important")
-
-X = data.loc[:, :"X20"].values
-Y = data["Y"]
-# cross validation:
-kf = cross_validation.KFold(len(Y), n_folds=5)
+# LINEAR REG
+kf = KFold(n_splits=5)
 acc = []
-for train_index, test_index in kf:
+for train_index, test_index in kf.split(X):
     LinReg.fit(X[train_index, :], Y[train_index])
     acc.append(LinReg.score(X[test_index, :], Y[test_index]))
 print(np.mean(acc))
 accuracies = pd.DataFrame(acc,
                     columns = ["LinReg"])
 
-kf = cross_validation.KFold(len(Y), n_folds=5)
+# LINEAR REG + UNIVARIATE FEATURE SELECTION
+selector = SelectPercentile(f_classif, percentile=20)
+selector.fit(X, Y)
+X_new = selector.transform(X)
+kf = KFold(n_splits=5)
 acc = []
-for train_index, test_index in kf:
+for train_index, test_index in kf.split(X):
+    LinReg.fit(X_new[train_index, :], Y[train_index])
+    acc.append(LinReg.score(X_new[test_index, :], Y[test_index]))
+print(np.mean(acc))
+accuracies["LinReg_feat_select"] = acc
+
+# RIDGE
+kf = KFold(n_splits=5)
+acc = []
+for train_index, test_index in kf.split(X):
     Ridge.fit(X[train_index, :], Y[train_index])
     acc.append(Ridge.score(X[test_index, :], Y[test_index]))
 print(np.mean(acc))
 accuracies["Ridge"] = acc
 
-kf = cross_validation.KFold(len(Y), n_folds=5)
+# LASSO
+kf = KFold(n_splits=5)
 acc = []
-for train_index, test_index in kf:
+for train_index, test_index in kf.split(X):
     Lasso.fit(X[train_index, :], Y[train_index])
     acc.append(Lasso.score(X[test_index, :], Y[test_index]))
 print(np.mean(acc))
 accuracies["Lasso"] = acc
+
+### Plot the resulting accuracies ###
 acc_stack = accuracies.stack().reset_index()
 del acc_stack["level_0"]
 acc_stack = acc_stack.rename(columns={0: 'acc', 'level_1':'param'})
@@ -72,33 +103,56 @@ plt.show()
 
 
 
-print("p = 50, 20 features are important")
+##################################################################
+### define specific dataset of p=50 with 20 important features ###
+##################################################################
+print("p = 50 with: 20 features are important")
 X = data.loc[:, :"X50"].values
 Y = data["Y"]
-# cross validation:
-kf = cross_validation.KFold(len(Y), n_folds=5)
+
+### Training and testing for each model ###
+
+# LINEAR REG
+kf = KFold(n_splits=5)
 acc = []
-for train_index, test_index in kf:
+for train_index, test_index in kf.split(X):
     LinReg.fit(X[train_index, :], Y[train_index])
     acc.append(LinReg.score(X[test_index, :], Y[test_index]))
 print(np.mean(acc))
 accuracies = pd.DataFrame(acc,
                     columns = ["LinReg"])
-                    
-kf = cross_validation.KFold(len(Y), n_folds=5)
+
+# LINEAR REG + UNIVARIATE FEATURE SELECTION
+selector = SelectPercentile(f_classif, percentile=20)
+selector.fit(X, Y)
+X_new = selector.transform(X)
+kf = KFold(n_splits=5)
 acc = []
-for train_index, test_index in kf:
+for train_index, test_index in kf.split(X):
+    LinReg.fit(X_new[train_index, :], Y[train_index])
+    acc.append(LinReg.score(X_new[test_index, :], Y[test_index]))
+print(np.mean(acc))
+accuracies["LinReg_feat_select"] = acc
+
+# RIDGE
+kf = KFold(n_splits=5)
+acc = []
+for train_index, test_index in kf.split(X):
     Ridge.fit(X[train_index, :], Y[train_index])
     acc.append(Ridge.score(X[test_index, :], Y[test_index]))
 print(np.mean(acc))
 accuracies["Ridge"] = acc
-kf = cross_validation.KFold(len(Y), n_folds=5)
+
+# LASSO
+kf = KFold(n_splits=5)
 acc = []
-for train_index, test_index in kf:
+for train_index, test_index in kf.split(X):
     Lasso.fit(X[train_index, :], Y[train_index])
     acc.append(Lasso.score(X[test_index, :], Y[test_index]))
 print(np.mean(acc))
 accuracies["Lasso"] = acc
+
+### Plot the resulting accuracies ###
 acc_stack = accuracies.stack().reset_index()
 del acc_stack["level_0"]
 acc_stack = acc_stack.rename(columns={0: 'acc', 'level_1':'param'})
@@ -110,34 +164,56 @@ plt.title("p = 50, 20 features associated with response")
 plt.tight_layout()
 plt.show()
 
-
-print("p = 1000, 20 features are important")
+####################################################################
+### define specific dataset of p=1000 with 20 important features ###
+####################################################################
+print("p = 1000 with: 20 features are important")
 X = data.loc[:, :"X1000"].values
 Y = data["Y"]
-# cross validation:
-kf = cross_validation.KFold(len(Y), n_folds=5)
+
+### Training and testing for each model ###
+
+# LINEAR REGRESSION
+kf = KFold(n_splits=5)
 acc = []
-for train_index, test_index in kf:
+for train_index, test_index in kf.split(X):
     LinReg.fit(X[train_index, :], Y[train_index])
     acc.append(LinReg.score(X[test_index, :], Y[test_index]))
 print(np.mean(acc))
 accuracies = pd.DataFrame(acc,
                     columns = ["LinReg"])
 
-kf = cross_validation.KFold(len(Y), n_folds=5)
+# LINEAR REG + UNIVARIATE FEATURE SELECTION
+selector = SelectPercentile(f_classif, percentile=20)
+selector.fit(X, Y)
+X_new = selector.transform(X)
+kf = KFold(n_splits=5)
 acc = []
-for train_index, test_index in kf:
+for train_index, test_index in kf.split(X):
+    LinReg.fit(X_new[train_index, :], Y[train_index])
+    acc.append(LinReg.score(X_new[test_index, :], Y[test_index]))
+print(np.mean(acc))
+accuracies["LinReg_feat_select"] = acc
+
+# RIDGE
+kf = KFold(n_splits=5)
+acc = []
+for train_index, test_index in kf.split(X):
     Ridge.fit(X[train_index, :], Y[train_index])
     acc.append(Ridge.score(X[test_index, :], Y[test_index]))
 print(np.mean(acc))
 accuracies["Ridge"] = acc
-kf = cross_validation.KFold(len(Y), n_folds=5)
+kf = KFold(n_splits=5)
 acc = []
-for train_index, test_index in kf:
+
+# LASSO
+for train_index, test_index in kf.split(X):
     Lasso.fit(X[train_index, :], Y[train_index])
     acc.append(Lasso.score(X[test_index, :], Y[test_index]))
 print(np.mean(acc))
 accuracies["Lasso"] = acc
+
+### Plot the resulting accuracies ###
 acc_stack = accuracies.stack().reset_index()
 del acc_stack["level_0"]
 acc_stack = acc_stack.rename(columns={0: 'acc', 'level_1':'param'})
@@ -148,14 +224,3 @@ plt.xlabel("")
 plt.title("p = 1000, 20 features associated with response")
 plt.tight_layout()
 plt.show()
-
-
-
-
-
-
-
-
-
-        
-        
